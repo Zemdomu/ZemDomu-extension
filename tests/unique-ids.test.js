85878bca277d2d1f02e408f37115947041890097
Zacharias Eryd Berlin <25438830+Zelcus@users.noreply.github.com>
2025-06-10 22:27:26 +0200

    Add bundle size test and update existing tests

--- tests/unique-ids.test.js
+++ tests/unique-ids.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const { lintHtml } = require('../out/linter');
 
+const options = { crossComponentAnalysis: false, rules: { uniqueIds: true, requireMain: false } };
 const html = '<div id="dup"></div><span id="dup"></span>';
-const res = lintHtml(html, false);
+const res = lintHtml(html, false, options);
 assert.ok(res.some(r => r.rule === 'uniqueIds'), 'Expected duplicate id warning');
 console.log('Unique id test passed');
